43abbffe33ce8bd58253d323fc742670d5d110da
Alexcmyk <alex.kirts@gmail.com>
2020-06-17 20:28:19 -0600

    adds typechecker functionality

--- typechecker.js
+++ typechecker.js
@@ -1,6 +1,11 @@
 const sentenceTag = document.querySelector(`input[type="text"]`);
 const outputTag = document.querySelector("section.output p");
+const originalText = outputTag.innerHTML;
 
 sentenceTag.addEventListener("keyup", function () {
-  outputTag.innerHTML = this.value;
+  if (this.value) {
+    outputTag.innerHTML = this.value;
+  } else {
+    outputTag.innerHTML = originalText;
+  }
 });
