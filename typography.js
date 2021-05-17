2359b7980840a25c98e9b3ae4bf238c77615eb15
Alexcmyk <alex.kirts@gmail.com>
2021-05-16 21:11:04 -0600

    initial redo commit

--- typography.js
+++ typography.js
@@ -12,10 +12,10 @@ const lineHeightOutput = document.querySelector(`span.leading-output`);
 const italicTag = document.querySelector(`input[name="italic"]`);
 
 // Color tags
-const colorTags = document.querySelectorAll("div.colors div");
+const colorTags = document.querySelectorAll('div.colors div');
 
 // Output tags
-const outputTag = document.querySelector("textarea.output");
+const outputTag = document.querySelector('textarea.output');
 const originalText = outputTag.value;
 
 // Type changer tags
@@ -26,61 +26,61 @@ const fontWeightTag = document.querySelector(`input[name="fontweight"]`);
 const fontWeightOutput = document.querySelector(`span.fontweight-output`);
 
 // Sentence and output logic
-sentenceTag.addEventListener("keyup", function () {
-  if (this.value) {
-    outputTag.value = this.value; //   Value is a hidden attribute that contains the content of the input tag
-  } else {
-    outputTag.value = originalText;
-  }
+sentenceTag.addEventListener('keyup', function () {
+    if (this.value) {
+        outputTag.value = this.value; //   Value is a hidden attribute that contains the content of the input tag
+    } else {
+        outputTag.value = originalText;
+    }
 });
 
 // Typing logic
-outputTag.addEventListener("keyup", function () {
-  sentenceTag.value = this.value;
+outputTag.addEventListener('keyup', function () {
+    sentenceTag.value = this.value;
 });
 
 // Typesize logic
-typeSizeTag.addEventListener("input", function () {
-  outputTag.style.fontSize = `${this.value}px`;
-  typeSizeOutput.innerHTML = `${this.value}px`;
+typeSizeTag.addEventListener('input', function () {
+    outputTag.style.fontSize = `${this.value}px`;
+    typeSizeOutput.innerHTML = `${this.value}px`;
 });
 
 // Line-height logic
-lineHeightTag.addEventListener("input", function () {
-  outputTag.style.lineHeight = this.value;
-  lineHeightOutput.innerHTML = this.value;
+lineHeightTag.addEventListener('input', function () {
+    outputTag.style.lineHeight = this.value;
+    lineHeightOutput.innerHTML = this.value;
 });
 
 // Checkbox logic
-italicTag.addEventListener("change", function () {
-  if (this.checked) {
-    outputTag.style.fontStyle = "italic";
-  } else {
-    outputTag.style.fontStyle = "normal";
-  }
+italicTag.addEventListener('change', function () {
+    if (this.checked) {
+        outputTag.style.fontStyle = 'italic';
+    } else {
+        outputTag.style.fontStyle = 'normal';
+    }
 });
 
 // Typeface logic
-typefaceTag.addEventListener("input", function () {
-  outputTag.style.fontFamily = this.value;
+typefaceTag.addEventListener('input', function () {
+    outputTag.style.fontFamily = this.value;
 });
 
 // Font-weight logic
-fontWeightTag.addEventListener("input", function () {
-  outputTag.style.fontWeight = this.value;
-  fontWeightOutput.innerHTML = this.value;
+fontWeightTag.addEventListener('input', function () {
+    outputTag.style.fontWeight = this.value;
+    fontWeightOutput.innerHTML = this.value;
 });
 
 // Color logic
 colorTags.forEach((tag) => {
-  tag.addEventListener("click", function () {
-    outputTag.style.backgroundColor = this.style.backgroundColor;
-    outputTag.style.color = this.style.color;
+    tag.addEventListener('click', function () {
+        outputTag.style.backgroundColor = this.style.backgroundColor;
+        outputTag.style.color = this.style.color;
 
-    // Add / remove class list
-    colorTags.forEach((tag) => {
-      tag.classList.remove("selected");
+        // Add / remove class list
+        colorTags.forEach((tag) => {
+            tag.classList.remove('selected');
+        });
+        this.classList.add('selected');
     });
-    this.classList.add("selected");
-  });
 });
